0cde8e92464359be0cb3a24116002c000c896418
Ryan H <rhendrickson11@mail.greenriver.edu>
2021-02-04 15:31:35 -0500

    added model/data-layer.php and completed our MVC. learned how to do arrays and expressions.

--- index.php
+++ index.php
@@ -5,17 +5,32 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-// require autoload file
+// required files
 require_once ('vendor/autoload.php');
+require_once ('model/data-layer.php');
 
 // create an instance of the base class (fat-free framework)
 $f3 = Base::instance();
 
 // define a default route (home page)
-$f3->route('GET /', function() {
-	// create a new view, then sends it to the client
+$f3->route('GET /', function($f3) {
+	// assign variables
+	$f3->set('username', 'jshmo');
+	$f3->set('password', sha1('Password01'));
+	$f3->set('title', 'Working with Templates');
+	$f3->set('ftemp', 67);
+	$f3->set('color', 'purple');
+	$f3->set('radius', 10);
+	$f3->set('pi', pi());
+	$f3->set('fruits', getFruits());
+	$f3->set('salaries', getSalaries());
+
+	// load template
 	$view = new Template();
 	echo $view->render('views/info.html');
+
+	// alternate syntax for loading template
+	// echo Template::instance()->render('views/info.html');
 });
 
 // run fat free HAS TO BE THE LAST THING IN FILE
---
+++ model/data-layer.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * //328/week5/model/data-layer.php
+ */
+
+// define functions to "get" data from somewhere
+function getFruits(): array {
+	// connect to DB
+
+	// read fruits
+
+	// process fruits
+
+	// return fruits
+	return array('banana', 'tomato', 'orange', 'apple', 'kiwi');
+}
+
+// returns an associative array of names and salaries
+function getSalaries(): array {
+	return array('jessica'=>200000, 'heather'=>250000, 'blezyl'=>300000);
+}
